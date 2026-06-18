e383fec22572dd2aa5b2567aa0d1cdab60b2de8a
Drisatech <drisatech@gmail.com>
2026-06-18 21:36:29 +0100

    modified server

--- server.ts
+++ server.ts
@@ -4,7 +4,12 @@ import fs from "fs";
 import { createServer as createViteServer } from "vite";
 import { GoogleGenAI, Type } from "@google/genai";
 import { Article, NewsSource, SystemLog, SystemConfig } from "./src/types.ts";
-import makeWASocket, { useMultiFileAuthState, DisconnectReason } from "@whiskeysockets/baileys";
+import * as baileysModule from "@whiskeysockets/baileys";
+
+// Robustly resolve Baileys exports to avoid CJS/ESM bundling issues
+const useMultiFileAuthState = baileysModule.useMultiFileAuthState;
+const DisconnectReason = baileysModule.DisconnectReason;
+const makeWASocket = (baileysModule.default || (baileysModule as any).makeWASocket || baileysModule) as any;
 import QRCode from "qrcode";
 import pino from "pino";
 
